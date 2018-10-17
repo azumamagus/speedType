bfdafb10bfee3936aff9afa2b2bba176069fdd7f
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2018-10-17 17:41:15 -0300

    Added Style and other features

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,79 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input",function(){
-    var conteudo = campo.val();
-    var qtdPalavras = conteudo.split(/\S+/).length -1;
-    $("#contador-palavras").text(qtdPalavras);
 
-    var qtdCaracteres = conteudo.length
-    $("#contador-caracteres").text(qtdCaracteres);
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras  = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/g).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+
+    });
+}
 //Controle de Tempo
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus",function(){
-    var cronometroId =setInterval(function(){
-        tempoRestante--;        
-        $("#tempo-digitacao").text(tempoRestante);
-        if(tempoRestante < 1){
-            campo.attr("disabled",true);
-            clearInterval(cronometroId);        
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        $("#botao-reiniciar").attr("disabled",true);
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+                $("#botao-reiniciar").attr("disabled", false);
+                campo.toggleClass("campo-desativado");
+            }
+        }, 1000);
+    });
+}
+
+function inicializaMarcadores() {
+
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
         }
-    },1000)
-});
+    });
+}
+
+
+$("#botao-reiniciar").click(reiniciaJogo);
+
+function reiniciaJogo(){
+    campo.attr("disabled",false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro(); 
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
