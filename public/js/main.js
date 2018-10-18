8ca86f3b75b88828834f6402ae0d37fc03fe0bc7
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2018-10-18 12:28:49 -0300

    Finished 1° part project

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
-$(function(){
+$(function() {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
@@ -13,6 +13,7 @@ function atualizaTamanhoFrase() {
     var frase = $(".frase").text();
     var numPalavras  = frase.split(" ").length;
     var tamanhoFrase = $("#tamanho-frase");
+
     tamanhoFrase.text(numPalavras);
 }
 
@@ -20,40 +21,21 @@ function inicializaContadores() {
     campo.on("input", function() {
         var conteudo = campo.val();
 
-        var qtdPalavras = conteudo.split(/\S+/g).length - 1;
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
         $("#contador-palavras").text(qtdPalavras);
 
         var qtdCaracteres = conteudo.length;
         $("#contador-caracteres").text(qtdCaracteres);
-
-    });
-}
-//Controle de Tempo
-function inicializaCronometro() {
-    var tempoRestante = $("#tempo-digitacao").text();
-    campo.one("focus", function() {
-        $("#botao-reiniciar").attr("disabled",true);
-        var cronometroID = setInterval(function() {
-            tempoRestante--;
-            $("#tempo-digitacao").text(tempoRestante);
-            if (tempoRestante < 1) {
-                campo.attr("disabled", true);
-                clearInterval(cronometroID);
-                $("#botao-reiniciar").attr("disabled", false);
-                campo.toggleClass("campo-desativado");
-            }
-        }, 1000);
     });
 }
 
 function inicializaMarcadores() {
-
     var frase = $(".frase").text();
     campo.on("input", function() {
         var digitado = campo.val();
-        var comparavel = frase.substr(0 , digitado.length);
+        var comparavel = frase.substr(0, digitado.length);
 
-        if(digitado == comparavel) {
+        if (digitado == comparavel) {
             campo.addClass("borda-verde");
             campo.removeClass("borda-vermelha");
         } else {
@@ -63,16 +45,33 @@ function inicializaMarcadores() {
     });
 }
 
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+    	var cronometroID = setInterval(function() {
+    		tempoRestante--;
+    		$("#tempo-digitacao").text(tempoRestante);
+    		if (tempoRestante < 1) {
+                clearInterval(cronometroID);
+                finalizaJogo();
+    		}
+    	}, 1000);
+    });
+}
 
-$("#botao-reiniciar").click(reiniciaJogo);
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
 
-function reiniciaJogo(){
-    campo.attr("disabled",false);
+function reiniciaJogo() {
+    campo.attr("disabled", false);
     campo.val("");
-    $("#contador-palavras").text("0");
-    $("#contador-caracteres").text("0");
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
     $("#tempo-digitacao").text(tempoInicial);
-    inicializaCronometro(); 
+    inicializaCronometro();
     campo.toggleClass("campo-desativado");
     campo.removeClass("borda-vermelha");
     campo.removeClass("borda-verde");
